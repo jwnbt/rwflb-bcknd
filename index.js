e5eeac9dc69d390adf72dd0a0e0699d27eddf6ad
jwnbt <jwnbtjun@gmail.com>
2022-03-16 18:15:30 -0500

    add route to sign on

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -14,9 +15,29 @@ app.get("/", (req, res) => {
 app.get("/goals", (req, res) => {
   const data = fs.readFileSync("./data.json");
   const goals = JSON.parse(data);
-  res.send(goals);
+  res.send(goals[1]);
+});
+
+app.post("/goals/:userName", bodyParser.json(), (req, res) => {
+  const userFolderPath = path.join("./app-data/users", req.params.userName);
+  const userDirExists = fs.existsSync(userFolderPath);
+  console.log("in");
+  console.log(userDirExists);
+  if (userDirExists) {
+    const userCredPath = path.join(userFolderPath, "cred.json");
+    const data = fs.readFileSync(userCredPath, "utf-8");
+    const parsedData = JSON.parse(data);
+    const credAreMatch =
+      req.body.username === parsedData.username &&
+      req.body.password === parsedData.password;
+    if (credAreMatch) {
+      return res.send({ hasAccess: true });
+    } else {
+      return res.send({ hasAccess: false });
+    }
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
