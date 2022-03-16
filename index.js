fc592cbac3091a0e14d200b17d306c30ff3c8dcc
jwnbt <jwnbtjun@gmail.com>
2022-03-16 14:01:42 -0500

    testing

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const fs = require("fs");
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("nothing here");
+});
+
 app.get("/goals", (req, res) => {
   const data = fs.readFileSync("./data.json");
   const goals = JSON.parse(data);
   res.send(goals);
-});
-
-app.get("/", (req, res) => {
-  res.send("hello world");
 });
 
 app.listen(PORT, () => {
