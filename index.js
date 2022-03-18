d663b448f6f64dad68f8a73e11b4e37c1e4c3b8a
jwnbt <jwnbtjun@gmail.com>
2022-03-18 11:08:27 -0500

    testing

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const PORT = process.env.PORT || 1000;
 const bodyParserJSON = bodyParser.json();
 const dataFilePath = "./data.json";
 
-app.use(cors({ origin: "https://wonderful-lovelace-495196.netlify.app/" }));
+app.use(
+  cors({
+    origin: "https://wonderful-lovelace-495196.netlify.app/",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 
 app.get("/", (req, res) => {
   res.send({ msg: "welcome" });
